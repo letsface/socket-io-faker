9c6a0917a25a068b8a2c63db6a5b2b7758fc3d01
rogerz <rogerz@letsface.com>
2014-05-22 14:46:30 +0800

    modify data format

--- test/recorder.js
+++ test/recorder.js
@@ -5,48 +5,72 @@ describe('recorder', function() {
   describe('normal conversation', function() {
     var rec = new recorder.Recorder();
     it('should record original message', function() {
-      rec.in('hello');
-      rec.out('world');
+      rec. in ('api', 'hello');
+      rec.out('api', 'world');
       expect(rec.records).to.eql([{
-        type: 'in',
-        msg: 'hello'
+        event: 'api',
+        direction: 'in',
+        data: 'hello'
       }, {
-        type: 'out',
-        msg: 'world'
+        event: 'api',
+        direction: 'out',
+        data: 'world'
       }]);
     });
-    it('should pair conversation', function () {
+    it('should pair conversation', function() {
       var conv = rec.conversation();
-      expect(conv).to.eql([{
-        in: 'hello',
-        out: 'world'
-      }]);
-    });
-  });
-  describe('in without out', function () {
-    var rec = new recorder.Recorder();
-    it ('should create new conversation', function () {
-      rec.in('hello1');
-      rec.in('hello2');
-      var conv = rec.conversation();
-      expect(conv).to.eql([{
-          in: 'hello1'
-        }, {
-          in: 'hello2'
+      expect(conv).to.eql([{ in : {
+          event: 'api',
+          data: 'hello',
+          direction: 'in'
+        },
+        out: {
+          event: 'api',
+          data: 'world',
+          direction: 'out'
         }
-      ]);
+      }]);
     });
   });
-  describe('out without in', function () {
+  describe('in without out', function() {
     var rec = new recorder.Recorder();
-    it('should ignore unpaired out', function () {
-      rec.in('hello');
-      rec.out('world');
-      rec.out('bye');
+    it('should create new conversation', function() {
+      rec.in ('api', 'hello1');
+      rec.in ('api', 'hello2');
       var conv = rec.conversation();
       expect(conv).to.eql([{
-        in: 'hello',
-        out: 'world'
+        in : {
+          event: 'api',
+          data: 'hello1',
+          direction: 'in'
+        }
+      }, {
+        in : {
+          event: 'api',
+          data: 'hello2',
+          direction: 'in'
+        }
+      }]);
+    });
+  });
+  describe('out without in', function() {
+    var rec = new recorder.Recorder();
+    it('should ignore unpaired out', function() {
+      rec.in ('api', 'hello');
+      rec.out('api', 'world');
+      rec.out('api', 'bye');
+      var conv = rec.conversation();
+      expect(conv).to.eql([{
+        in : {
+          event: 'api',
+          direction: 'in',
+          data: 'hello'
+        },
+        out: {
+          event: 'api',
+          direction: 'out',
+          data: 'world'
+        }
       }]);
     });
   });
